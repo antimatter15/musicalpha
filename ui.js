6fbae11bdc98376cd72ed0b660aca2920f42e3f5
antimatter15 <antimatter15@gmail.com>
2012-02-04 21:56:06 -0500

    it looks quite purty

--- ui.js
+++ ui.js
@@ -3,19 +3,21 @@ function updatePolar(p1, p2){
 	var ctx = document.getElementById("polar").getContext('2d')
 	ctx.clearRect(0, 0, 300, 300);
 	ctx.beginPath()
-	ctx.arc(150, 150, 130, Math.PI * (-.5), Math.PI * (-.5 + 2 * p1) , false);
+	ctx.arc(150, 150, 120, Math.PI * (-.5), Math.PI * (-.5 + 2 * p1) , false);
 	ctx.strokeStyle = "#aaa"
 	ctx.lineWidth = 15;
 	ctx.stroke()
 
 	ctx.beginPath()
 	//invert polar state every revolution!
-	ctx.arc(150, 150, 90, Math.PI * (-.5), Math.PI * (-.5 - 2 * p2) , !polar_state);
+	ctx.arc(150, 150, 80, Math.PI * (-.5), Math.PI * (-.5 - 2 * p2) , !!polar_state);
 	ctx.strokeStyle = "#ccc"
 	ctx.lineWidth = 15;
 	ctx.stroke()
 
 	ctx.font = '14pt Syncopate'
+	//adding text makes a bit too much noise for my taste
+
 	//ctx.fillText((p2*100).toFixed(0) + '%', 150 - 10, 150 - 90 + 7)
 	//ctx.fillText('%', 150 - 10, 150)
 
